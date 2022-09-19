20e7771ebb1ceefb883604fea2b8ee9e6fba8e0b
jcrocker-cmd <narbajajc@gmail.com>
2022-09-20 00:16:29 +0800

    Updated Dashboard Using Bootstrap

--- resources/views/dashboard/assets/script.blade.php
+++ resources/views/dashboard/assets/script.blade.php
@@ -1,1 +1,5 @@
 <script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- <script src="jquery.js"></script> -->
+<script src="ul-active.js"></script>
+<script src="dashboard-toggle.js"></script>
